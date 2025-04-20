32296af6d9a40e3ce92d150cbd51f63aa41abf9c
Freddy <freddy@mbf.pe>
2025-04-20 10:55:52 -0500

    update Astro configuration and package scripts for improved database integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import db from '@astrojs/db';
 import vue from '@astrojs/vue';
 
 import node from '@astrojs/node';
+
+import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
 
   integrations: [mdx({
     extendMarkdownConfig: false,
-  }), sitemap(), db(), vue()],
+  }), sitemap(), vue(), db()],
 
   output: "static",
   experimental: {
